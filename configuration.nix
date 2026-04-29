280e87bde9ac2099cfe4644b10a7dd70afd89c4b
Ben Foster <dev@athomeadmin.net>
2026-04-29 15:01:56 -0600

    Updated configuration.nix To Properly Load Greetd

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --cmd hyprland";
         user = "greeter";
       };
     };
