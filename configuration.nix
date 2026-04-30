ccd3835f2cac7dec95bec74d22c15e1bbeb9aea1
Ben Foster <dev@athomeadmin.net>
2026-04-30 00:19:25 -0600

    ALPHA BUILD - Fixed Issues With Keybind For Zen Browser

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,11 @@
   # Optional: Recommended for Electron apps to use Wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   # Enable Tui-Greet As Login Manager
   services.greetd = {
     enable = true;
