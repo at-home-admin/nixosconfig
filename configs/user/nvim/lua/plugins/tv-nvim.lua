cc56c65d72e79ba9bfdffeac49daebf19232bb72
Ben Foster <dev@athomeadmin.net>
2026-05-01 22:48:50 -0600

    ALPHA BUILD - Added Keybind (SUPER N) To Launch Television Inside NixOS Config Folder

---
+++ configs/user/nvim/lua/plugins/tv-nvim.lua
@@ -0,0 +1,8 @@
+return {
+    "alexpasmantier/tv.nvim",
+    config = function()
+        require("tv").setup({
+            -- your config here (see Configuration section below)
+        })
+    end,
+}
