19d296e6a2842e8e4378d55611e2c8ecaa168036
Ben Foster <dev@athomeadmin.net>
2026-06-03 04:26:23 -0600

    XANTUSIA BUILD - Added new database url to dadbod

--- configs/user/nvim/lua/plugins/nvim-ufo.lua
+++ configs/user/nvim/lua/plugins/nvim-ufo.lua
@@ -1,7 +1,26 @@
 return {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-        "kevinhwang91/promise-async",
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = "kevinhwang91/promise-async",
         "nvim-lua/plenary.nvim",
+        event = "BufReadPost", -- Load when opening a file to maintain performance
+        config = function()
+            -- Neovim folder options required by UFO
+            vim.o.foldcolumn = "1" -- Shows a visual column on the left side
+            vim.o.foldlevel = 99 -- High default so files don't start fully collapsed
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            -- Keymaps to open and close folds globally
+            vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+            vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+
+            -- Set up the plugin to use Treesitter as the folding provider
+            require("ufo").setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return { "treesitter", "indent" }
+                end,
+            })
+        end,
     },
 }
