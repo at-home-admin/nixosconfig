2742906d56bbdc9cad3ad8f891e2554437d937a5
Ben Foster <dev@athomeadmin.net>
2026-06-10 06:50:56 -0600

    YARARA BUILD - Added tzlocal to configuration build to use sqlfluff in neovim

--- configuration.nix
+++ configuration.nix
@@ -175,6 +175,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    (python3.withPackages (ps: [
+      ps.tzlocal
+      ps.pytz
+    ]))
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
