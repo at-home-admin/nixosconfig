d7fbc0965dba6a7d88f0e44f99a2c0286b8fab51
Ben Foster <dev@athomeadmin.net>
2026-06-15 11:00:49 -0600

    YARARA BUILD - Updated configuration for conform.nvim

---
+++ configs/user/nvim/lua/plugins/conform-nvim.lua
@@ -0,0 +1,25 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "LspAttach", "BufReadPost", "BufNewFile" },
+    opts = {
+        formatters_by_ft = {
+            python = { "ruff_format" },
+            vue = { "prettier" },
+        },
+        format_on_save = {
+            timeout_ms = 2500,
+            lsp_fallback = true,
+        },
+    },
+    config = function(_, opts)
+        local conform = require("conform")
+
+        -- Setup "conform.nvim" to work
+        conform.setup(opts)
+
+        -- Customise the default "prettier" command to format Markdown files as well
+        conform.formatters.prettier = {
+            prepend_args = { "--prose-wrap", "always" },
+        }
+    end,
+}
